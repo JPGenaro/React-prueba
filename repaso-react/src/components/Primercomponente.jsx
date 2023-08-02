1873ea13cd8d2d2393d031b857d0e4f4a1a53997
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-08-02 10:36:31 -0300

    Detalles en componentes

--- repaso-react/src/components/Primercomponente.jsx
+++ repaso-react/src/components/Primercomponente.jsx
@@ -4,6 +4,16 @@ export const Primercomponente = () => {
 
     let nombre = "Carlitos";
     let web = "carlitosdou.es";
+
+    let cursos = [
+        "Master en python",
+        "Master en Javascrips",
+        "Master en MySql"
+    ]
+
+    const cambiarNombre = (nuevoNombre) => {
+        nombre = nuevoNombre;
+    }
  
   return (
     <div>
@@ -11,6 +21,19 @@ export const Primercomponente = () => {
         <p>Este es el primer Componente de React...</p>
         <p>Mi nombre es: {nombre}</p>
         <p>Mi web es: {web}</p>
+
+        <button onClick={ e => cambiarNombre("CARLITOS")}></button>
+
+        <h2>Cursos: </h2>
+        <ul>
+            {
+                cursos.map( (curso, indice) => {
+                    return (<li key={indice}>
+                            {curso}
+                    </li>)
+                })
+            }
+        </ul>
     </div>
   )
 }
