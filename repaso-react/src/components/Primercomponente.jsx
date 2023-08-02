c2312d5be64695e87e90379d57ae19bd05d0a575
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-08-02 10:29:38 -0300

    Agragando componentes

--- repaso-react/src/components/Primercomponente.jsx
+++ repaso-react/src/components/Primercomponente.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 export const Primercomponente = () => {
+
+    let nombre = "Carlitos";
+    let web = "carlitosdou.es";
+ 
   return (
-    <div>Primercomponente</div>
+    <div>
+        <h1>Primer Componente</h1>
+        <p>Este es el primer Componente de React...</p>
+        <p>Mi nombre es: {nombre}</p>
+        <p>Mi web es: {web}</p>
+    </div>
   )
 }
