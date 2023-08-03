b7212c8820c59b964f1721c80c94231d4ba7fad9
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-08-03 09:27:18 -0300

    Terminado

--- repaso-react/src/components/Primercomponente.jsx
+++ repaso-react/src/components/Primercomponente.jsx
@@ -19,12 +19,14 @@ export const Primercomponente = () => {
     <div>
         <h1>Primer Componente</h1> 
         <p>Este es el primer Componente de React...</p>
-        <p>Mi nombre es: {nombre}</p>
+        <p>Mi nombre es: <strong className={nombre.length >= 4 ? 'verde' : 'rojo'}>{nombre}</strong></p>
         <p>Mi web es: {web}</p>
 
         <input type="text" onChange={e => cambiarNombre(e.target.value)} placeholder='Cambia el nombre' />
 
         <button onClick={ e => cambiarNombre("CARLITOS MAIDANA")}>Cambiar</button>
+
+        <button onClick={e => {console.log("El valor guardado en tu estado es : ", nombre);}}>Mostrar valor de estado</button>
 
         <h2>Cursos: </h2>
         <ul>
